6891b370c22b9975b14502c06979e1b265f07f23
alexx <alexxfiuza33@gmail.com>
2023-08-10 21:51:43 -0300

    Finishing the challenge

--- script.js
+++ script.js
@@ -9,5 +9,53 @@ function ToggleMode() {
   } else {
     img.setAttribute("src", "./assets/zertwoavatar.png")
   }
-}
 
+
+  const trd = document.querySelector("a#yt")
+
+    if (html.classList.contains("light")) {
+    trd.setAttribute("href", "https://www.instagram.com/fiuza.10/")
+    } 
+    else {
+    trd.setAttribute(
+      "href",
+      "https://www.youtube.com/channel/UCs5uwfKbgCSB-12-VwWNq4g"
+    )
+    }
+
+
+
+  const gen = document.querySelector("ion-icon#gen1")
+
+  if (html.classList.contains("light")) {
+    gen.setAttribute("name", "logo-youtube")
+  }
+
+  else {
+  gen.setAttribute("name", "logo-instagram")
+  }
+
+
+  const trd2 = document.querySelector("a#ig")
+
+    if (html.classList.contains("light")) {
+      trd2.setAttribute(
+        "href",
+        "https://www.youtube.com/channel/UCs5uwfKbgCSB-12-VwWNq4g"
+      )
+    }
+    else{
+      trd2.setAttribute("href", "https://www.instagram.com/fiuza.10/")
+    }
+
+  
+  const gen1 = document.querySelector("ion-icon#gen")
+
+  if (html.classList.contains("light")) {
+    gen1.setAttribute("name", "logo-instagram")
+  } 
+  
+  else {
+    gen1.setAttribute("name", "logo-youtube")
+  }
+}
